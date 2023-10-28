764d18bcbfafb6e4f34d329e50f201779415046c
Karthik Krishnan K K <110273701+KarthikKKrishnan@users.noreply.github.com>
2023-10-28 10:08:48 +0530

    Update router.js

--- router.js
+++ router.js
@@ -7,3 +7,16 @@ const router = Router();
 router.route("/").get(rh.setData);
 
 export default router;
+
+
+
+
+import { Router } from "express";
+import * as rh from "./request-handler.js";
+import fs from "fs";
+
+const router = Router();
+
+router.route("/").get(rh.getData).post(rh.addTodo).delete(rh.deleteTodo).put(rh.editTodo);
+
+export default router;
