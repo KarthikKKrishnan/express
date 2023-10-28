e39a030e0390988b70944f52f0cbb53599a41693
Karthik Krishnan K K <110273701+KarthikKKrishnan@users.noreply.github.com>
2023-10-28 10:08:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,29 @@ server.use("/set-todo" ,(req, res)=>{
    
 })
 
+
+import express from "express";
+import router from "./router.js";
+import fs from "fs";
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use("/", express.static("./static"));
+
+server.use("/api", router);
+
+server.listen(3000, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log("Server started on port 3000");
+});
+
+
 //  server.get("/api",(req,res) => {
 //     console.log(req.query);
 //     res.json("hello");
